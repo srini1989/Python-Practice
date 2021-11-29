2dae0515f936bb9af3a2c3a36d546490607c5b73
Srini-Mac <14941850+srini1989@users.noreply.github.com>
2021-11-29 12:06:51 -0800

    End of 1st Chapter

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# Python-Practice-Programs-3.9.0
+# Python-Practice-Programs-3.9
 
 This Repository contains a set of basic programs written in Python 3.9.
---
+++ boiling.py
@@ -0,0 +1,4 @@
+boiling_fh = 333.0
+fahrenheit = boiling_fh
+celsius = (fahrenheit - 32) * 5 / 9
+print("Boiling point = {}F or {} C".format(fahrenheit,celsius))
